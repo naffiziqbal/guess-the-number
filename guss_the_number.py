c9f99c78de58263c68069c4e1e0384353adb2c13
nafiziqbal <68105275+naffiziqbal@users.noreply.github.com>
2022-04-24 23:33:10 +0600

    Add files via upload

---
+++ guss_the_number.py
@@ -0,0 +1,34 @@
+# Ïmporting The Resource
+
+import random
+
+
+# Asking The User Wheter Play or Not
+playing = input("So, Let's Start The Game? Y|N ")
+if playing.lower() != "y":
+    quit("Okay! We Will Play Again")
+
+else:
+    print("Let's Get Started")
+
+comp_guss = random.randrange(100)
+attempts = 0
+
+
+while True:
+    user_guess = int(input("Guess a Number Between 1 - 100 ")) # Taking Inputs From the user
+    if user_guess == comp_guss:
+        print("You Won")
+        attempts += 1
+        break
+    if user_guess > comp_guss:
+        print("Try With a lower Number")
+        attempts += 1
+    if user_guess < comp_guss:
+        print("Try again With a Bigger Number")
+        attempts += 1
+
+    else:
+        print("Not a number")
+
+print(attempts)
